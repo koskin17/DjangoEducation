936e768cbf411577dcf133c751c75345e2d39208
koskin <kostia.zeykin@gmail>
2025-07-17 14:04:37 +0300

    make migrations to Database and set up routes to images

--- education/education/urls.py
+++ education/education/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 # Функция include позволяет подключать списки маршрутов из приложений, которые хранятся в файлах urls.py в самих приложения
@@ -23,3 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls'), name='news'), # Если используется список маршрутов (include), то django отбрасыввет не только домен, но и первый параметр, 'news/' в данном случае. Т.е. в итоге получается пустая строка.
 ]
+
+# Эта строка добавлена для того, чтобы в режиме DEBUG (он автоматически включен при разработке на локальной машине выше в переменной DEBUG) иметь прямой досуп к загружаемым изображениемя. При вырузке на сервер это уже будет обрабатывать сам сервер.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
