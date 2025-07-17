f8ac30e5b748e2de1078f289ecc5c7c85c8e5ef5
koskin <kostia.zeykin@gmail>
2025-07-17 16:04:11 +0300

    add first templates and desplayed data on page

--- education/news/urls.py
+++ education/news/urls.py
@@ -4,5 +4,4 @@ from .views import *
 
 urlpatterns = [
     path('', index),    # Из-за того, что в корневом файле views.py есть include со списком маршрутов и path с 'news/' был отброшен, то здесь мы указываем пустую строку, а потом функцию, которую вызываем
-    path('test/', test)
 ]
--- education/news/views.py
+++ education/news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Hello world!')
+from .models import News
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+def index(request):
+    news = News.objects.order_by('-created_at')  # Получаем все новости, отсортированные по дате создания в обратном порядке
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+    # В функциб render мы передаём сам запрос, путь к файлу шаблона, а также можно передать переменные и их значения, которые потом будут доступны на странице по своим именам
