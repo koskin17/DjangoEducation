40bf0ea31d6f101c3c7c5ee2d21cffceba06c34f
koskin <kostia.zeykin@gmail>
2025-07-22 14:54:20 +0300

    added template inheritance

--- education/news/urls.py
+++ education/news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),    # Из-за того, что в корневом файле views.py есть include со списком маршрутов и path с 'news/' был отброшен, то здесь мы указываем пустую строку, а потом функцию, которую вызываем
-    path('category/<int:category_id>/', get_category),
+    path('', index, name='home'),    # Из-за того, что в корневом файле views.py есть include со списком маршрутов и path с 'news/' был отброшен, то здесь мы указываем пустую строку, а потом функцию, которую вызываем
+    path('category/<int:category_id>/', get_category, name='category'),
 ]
