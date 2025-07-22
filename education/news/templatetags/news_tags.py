81f1c65155e8af74ba9bdb0e482307200760702b
koskin <kostia.zeykin@gmail>
2025-07-22 15:51:20 +0300

    custom template tags

---
+++ education/news/templatetags/news_tags.py
@@ -0,0 +1,24 @@
+# Создаём пользовательские / свои собсвтенные теги.
+from django import template
+
+from news.models import Category
+
+# Регистрируем наш template тег, чтобы всё работало
+register = template.Library()
+
+
+# Теперь создаём функцию, которая будет возвращать наши категории, которая и будет тем самым пользовательским тегом
+# Также важно использовать декоратор. Можно без скобок, но в скобки можно передать новое название функции (вместо get_categories) в виде параметра name = '')
+# Это и есть собственный, кастомный simple_tag
+@register.simple_tag(name='list_categories')
+def get_categories():
+    """ Функция возвращает список категорий"""
+
+    return Category.objects.all()
+
+# Создаём inclusion_tag
+# Сначала регистрируем декоратором шаблон, который мы создаём предварительно
+@register.inclusion_tag('news/list_categories.html')
+def show_categories(arg1='Hello', arg2='World'):
+    categories = Category.objects.all()
+    return {"categories": categories, "arg1": arg1, "arg2": arg2}
--- education/news/views.py
+++ education/news/views.py
@@ -5,18 +5,15 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()  # Получаем все новости, отсортированные по дате создания в обратном порядке. Сотрировка берётся из модели, из класса Meta
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список категорий',
-        'categories': categories,
     }
     return render(request, template_name='news/index.html', context=context)
     # В функциб render мы передаём сам запрос, путь к файлу шаблона, а также можно передать переменные и их значения, которые потом будут доступны на странице (в html-шаблоне) по своим именам
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', { 'news': news, 'categories': categories, 'category': category})
+    return render(request, 'news/category.html', { 'news': news, 'category': category})
 
