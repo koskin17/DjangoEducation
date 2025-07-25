d6ff5b49527792de127801d702b7833dd286f7ca
koskin <kostia.zeykin@gmail>
2025-07-25 16:09:50 +0300

    ListView

--- education/news/urls.py
+++ education/news/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),    # Из-за того, что в корневом файле views.py есть include со списком маршрутов и path с 'news/' был отброшен, то здесь мы указываем пустую строку, а потом функцию, которую вызываем
+    # path('', index, name='home'),    # Из-за того, что в корневом файле views.py есть include со списком маршрутов и path с 'news/' был отброшен, то здесь мы указываем пустую строку, а потом функцию, которую вызываем
+    path('', HomeNews.as_view(), name='home'),    # Это маршрут для переопределнного класса HomeNews
     path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
     path('news/add-news/', add_news, name='add_news'),
